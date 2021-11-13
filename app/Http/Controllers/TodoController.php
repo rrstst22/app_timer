21ccea45be233f6bb73103ec7268d5a6ceafa79a
rrstst22 <tsrrryou@gmail.com>
2021-11-14 02:00:54 +0900

    コントローラー修正

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -22,6 +22,21 @@ class TodoController extends Controller
 
     public function registerTodo(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string|max:10'
+        ],
+        [
+            'date' => 'required|string|max:10'
+        ],
+        [
+            'name.required' => 'イベント名は必須です。',
+            'name.sting'  => '文字列を入力してください。',
+            'name.max'  => '最大max文字です。',
+            'date.required' => 'イベント名は必須です。',
+            'date.sting'  => '日付を入力してください',
+            'date.max'  => '日付を入力してください',
+        ]);
+
         Todo::create(
             array(
                 'date' => new DateTime($request->date),
